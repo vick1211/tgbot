760ca3ba2ebc67bb05d4bd7f54e5f37ea5ed4516
vick1211 <nikitakotkov66@gmail.com>
2024-01-07 17:55:28 +0500

    small changes

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from config_data.config import load_config
-from handlers import user_handlers
+from handlers import user_handlers, other_handlers
 
 async def main() -> None:
     config = load_config(r'C:\python\tgbot\.env')
@@ -11,6 +11,7 @@ async def main() -> None:
     dp = Dispatcher()
 
     dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
