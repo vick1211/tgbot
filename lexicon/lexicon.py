408b7e2cf990aca8ef33b87cd43617acd658c5ac
vick1211 <nikitakotkov66@gmail.com>
2024-01-07 17:46:55 +0500

    add text for menu commands

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -1,3 +1,10 @@
 ru_lexicon: dict[str, str|list[str]] = {
-    '/start':'Этот бот не работает. Пока что'
+    '/start':'Это тестовая версия бота',
+    '/help':'Эта кнопка пока в разработке',
+    '/delmenu':'Кнопка "Menu" удалена. Если она продолжает отображаться, перезайдите в Telegram'
+}
+
+ru_menuCommands_lexicon: dict[str, str] = {
+    '/help':'Помощь',
+    '/delmenu':'Удалить меню'
 }
