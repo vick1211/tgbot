b9f301c9f5b402d3c0beb667a5fc20d131527c53
vick1211 <nikitakotkov66@gmail.com>
2024-02-04 17:02:35 +0500

    add not declared command warning

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,11 +1,15 @@
 from aiogram import Router, Bot
 from aiogram.types import Message
 from aiogram.filters import Command
-from lexicon.lexicon import ru_lexicon
+from lexicon.lexicon import ru_commands_lexicon
 
 router = Router()
 
 @router.message(Command(commands='delmenu'))
 async def del_main_menu(message: Message, bot:Bot) -> None:
     await bot.delete_my_commands()
-    await message.answer(text=ru_lexicon['/delmenu'])
+    await message.answer(text=ru_commands_lexicon['/delmenu'])
+
+@router.message()
+async def warning_not_declared_command(message: Message):
+    await message.answer(text='Извините, моя твоя не понимать')
