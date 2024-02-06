7e41651749f339da122528c4f000042a328fcd96
vick1211 <nikitakotkov66@gmail.com>
2024-02-06 19:44:39 +0500

    created new keyboards

--- keyboards/keyboard.py
+++ keyboards/keyboard.py
@@ -1,7 +1,24 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-keyboard: list[list[KeyboardButton]] = []
+keyboard1: list[list[KeyboardButton]] = []
 
-keyboard.append([KeyboardButton(text='Расписание')])
+keyboard1.append([KeyboardButton(text='Расписание')])
+keyboard1.append([KeyboardButton(text='Параметры')])
 
-my_keyboard = ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+studentKeyboard = ReplyKeyboardMarkup(keyboard=keyboard1, resize_keyboard=True)
+
+keyboard1.append([KeyboardButton(text='Отправить сообщение')])
+
+teacherKeyboard = ReplyKeyboardMarkup(keyboard=keyboard1, resize_keyboard=True)
+
+keyboard2: list[list[KeyboardButton]] = []
+
+keyboard2.append([KeyboardButton(text='Мои данные')])
+keyboard2.append([KeyboardButton(text='Удалить учетную запись')])
+keyboard2.append([KeyboardButton(text='Назад')])
+
+parametersKeyboard = ReplyKeyboardMarkup(keyboard=keyboard2, resize_keyboard=True)
+
+keyboard3 = []
+
+noneKeyboard = ReplyKeyboardMarkup(keyboard=keyboard3)
